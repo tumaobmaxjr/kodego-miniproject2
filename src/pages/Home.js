bcee13c2c50cb4b2d4b4fa07c971507565d4dacc
Michael <michaeldrpabaya@gmail.com>
2022-08-08 13:57:38 +0800

    removed uneccesary files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import PeriodicData from "./periodictable/PeriodicData";
-import PageTwo from "./periodictable/PageTwo";
+import ElementInfo from "./periodictable/ElementInfo"
+
+import {Center  } from '@chakra-ui/react'
 
 const Home = () => {
     return (
         <>
             <PeriodicData />
-            <PageTwo />
+            <Center id="page-two-header" my={'2rem'} pb={{ base: '0.7em', md: '0' }} mx={{ base: '10%', md: '20%' }} fontSize={{ base: '1em', md: '1.5em' }} >LIST OF GROUP ELEMENTS WITH CHEMICAL GROUP BLOCK</Center>
+            <ElementInfo/>
         </>
     );
 }
